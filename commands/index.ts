0a021686e159f7885e60374ff6372100f0d4b433
Tiago Ramos <tiago_bras_ramos@hotmail.com>
2025-04-05 18:30:22 +0100

    Added logs to prod test

--- commands/index.ts
+++ commands/index.ts
@@ -7,13 +7,18 @@ import path from "path";
 function getCommands(commandsPath: string) {
     const commands: Map<string, CommandHandler> = new Map();
     const isCompiled = __dirname.endsWith("dist");
+    console.log("getCommands")
+    console.log(__dirname);
+    console.log(isCompiled);
 
     const basePath = !isCompiled
         ? path.join(__dirname, commandsPath)
         : path.join(__dirname, "commands", commandsPath);
+
+    console.log(basePath);
     fs.readdirSync(basePath).forEach((file) => {
         if (file.endsWith(".ts") || file.endsWith(".js")) {
-            const command = require(`./${isCompiled && 'commands/'}${commandsPath}/${file}`);
+            const command = require(`./${isCompiled ? 'commands/' : ''}${commandsPath}/${file}`);
             commands.set(command.name ?? file.replace(".ts", "").replace(".js", ""), command);
         }
     });
